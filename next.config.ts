1053079f522ce3d9595f7bc0c176cf0743db4826
Harrison <55332974+HarrisonEXE@users.noreply.github.com>
2026-04-24 14:40:18 -0400

    Resolve next config bug

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       {
         hostname: 'picsum.photos',
         protocol: 'https'
+      },
+      {
+        hostname: 'raw.githubusercontent.com',
+        protocol: 'https'
       }
     ]
   }
